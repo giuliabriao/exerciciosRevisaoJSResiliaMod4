7c054767c77649f4a91c75920c543327b8154398
giuliabriao <briaogiulia@gmail.com>
2021-04-28 23:03:37 -0300

    Exercicio refeitos

--- ParteB/q3.js
+++ ParteB/q3.js
@@ -2,8 +2,9 @@
 const arr = [1, 2, 3];
 const arr2 = [4, 5, 6];
 
-const [um, ...array1] = arr
+const [item1Arr1, ...array1] = arr
+const [item1Arr2, ...array2] = arr2
 
-const allArr = [...array1, ...arr2]
+const allArr = [...array1, ...array2]
 
 console.log(allArr);
